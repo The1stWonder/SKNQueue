72005a72e321efe2b9f977b449c456edf65a6599
Sirapop <f.sirapop@gmail.com>
2017-05-23 21:10:22 +0700

    FIrst Add

---
+++ MasterQ/View/SummaryPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MasterQ
+{
+	public partial class SummaryPage : ContentPage
+	{
+		public SummaryPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
