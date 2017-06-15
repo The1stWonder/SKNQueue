18ff8be7d969c3b03bac34abed08a118c319674e
Kooms <koomshou@hotmail.com>
2017-06-15 22:19:14 +0700

    Update Login - James

--- MasterQ/View/LoginPage.xaml.cs
+++ MasterQ/View/LoginPage.xaml.cs
@@ -12,10 +12,19 @@ namespace MasterQ
 			InitializeComponent();
 		}
 
+
 		void Login_Clicked(object sender, System.EventArgs e)
 		{
+			
 			var username = mUsernameEntry.Text;
 			var password = mPasswordEntry.Text;
+			Validation varEmail = new Validation(username);
+			Validate.validateEmail(varEmail);
+			if(!varEmail.callBack.isSuccess)
+			{
+                DisplayAlert("Click", varEmail.callBack.message, "Close");
+			}
+
 			Navigation.PushAsync(new MainPage());
 		}
 		void Account_Clicked(object sender, System.EventArgs e)
