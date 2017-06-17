caf2e5844ddb3534f9cc52cad1eab9e1b0414be5
First Sirapop <f.sirapop@gmail.com>
2017-06-17 21:17:41 +0700

    Test Commit

--- MasterQ/Common/Constants.cs
+++ MasterQ/Common/Constants.cs
@@ -3,7 +3,7 @@ namespace MasterQ
 {
 	public static class Constants
 	{
-		public static String authenFail = "Login Failed. Username Or Password incorrect!!!";
+		public static String authenFail = "Login Failed. Username Or Password incorrect";
 		public static String emptyUserName = "Please input username";
 		public static String emptyPassword = "Please input password";
 		public static String emptyEmail = "Please input email";
