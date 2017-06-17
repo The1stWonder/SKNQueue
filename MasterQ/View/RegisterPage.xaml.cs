baa3cf4ba43b446f1806a87200eb7807abe55f5c
Kooms <koomshou@hotmail.com>
2017-06-17 19:37:56 +0700

    register

--- MasterQ/View/RegisterPage.xaml.cs
+++ MasterQ/View/RegisterPage.xaml.cs
@@ -11,5 +11,15 @@ namespace MasterQ
 		{
 			InitializeComponent();
 		}
+
+		void Submit_Clicked(object sender, System.EventArgs e)
+		{
+			var name = mNameEntry.Text;
+			var Email = mEmailEntry.Text;
+			var Password1 = mPasswordEntry.Text;
+			var Password2 = mPassword2Entry.Text;
+
+			Navigation.PushAsync(new MainPage());
+		}
 	}
 }
