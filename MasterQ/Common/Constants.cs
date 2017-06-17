4e45d37bf7559fe1ab6a3456b7096da7436f7e2d
First Sirapop <f.sirapop@gmail.com>
2017-06-17 21:12:01 +0700

    F

--- MasterQ/Common/Constants.cs
+++ MasterQ/Common/Constants.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 namespace MasterQ
 {
-    public static class Constants
-    {
-        public static String authenFail = "Login Failed. Username Or Password incorrect!!!";
+	public static class Constants
+	{
+		public static String authenFail = "Login Failed. Username Or Password incorrect!!!";
 		public static String emptyUserName = "Please input username";
 		public static String emptyPassword = "Please input password";
-        public static String emptyEmail = "Please input email";
-        public static String notSamePassword = "Please input same password";
+		public static String emptyEmail = "Please input email";
+		public static String notExistingEmail = "This email is not register";
+		public static String notSamePassword = "Please input same password";
 		public static String invalidEmail = "Invalid Email Format. For Example aaa@bbb.com";
-    }
+	}
 }
