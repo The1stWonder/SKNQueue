9e9f854ca1723f076b683249906e93b316706393
JamesPe <koomshou@hotmail.com>
2017-06-17 20:56:51 +0700

    FogetPassword

---
+++ MasterQ/View/ForgetPassword.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MasterQ
+{
+	public partial class ForgetPassword : ContentPage
+	{
+		public ForgetPassword()
+		{
+			InitializeComponent();
+		}
+
+		void Submit_Clicked(object sender, System.EventArgs e)
+		{
+			var Email = mEmailEntry.Text;
+
+			Navigation.PushAsync(new LoginPage());
+		}
+	}
+}
