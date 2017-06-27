d1b9a838a34be82bc9898cead8a70d370f1248f2
First Sirapop <f.sirapop@gmail.com>
2017-06-27 19:55:00 +0700

    Edit profile controller

--- MasterQ/Controller/EditProfileController.cs
+++ MasterQ/Controller/EditProfileController.cs
@@ -3,8 +3,53 @@ namespace MasterQ
 {
 	public class EditProfileController
 	{
-		public EditProfileController()
+		public EditProfileController(Member input)
 		{
+			if (isEmptyEmail(input)) return;
+			if (isEmptyPassword(input)) return;
+			if (isEmptyConfirmPassword(input)) return;
+			if (isEmptyMemberName(input)) return;
+			if (isValidEmail(input)) return;
+			if (isSamePassword(input)) return;
 		}
+		private static bool isEmptyEmail(Member input)
+		{
+			bool ret = String.IsNullOrEmpty(input.email);
+			input.callBack.setReturn(!ret, String.Empty, Constants.emptyEmail);
+			return ret;
+		}
+		private static bool isEmptyPassword(Member input)
+		{
+			bool ret = String.IsNullOrEmpty(input.password);
+			input.callBack.setReturn(!ret, String.Empty, Constants.emptyPassword);
+			return ret;
+		}
+		private static bool isEmptyConfirmPassword(Member input)
+		{
+			bool ret = String.IsNullOrEmpty(input.password);
+			input.callBack.setReturn(!ret, String.Empty, Constants.emptyPassword);
+			return ret;
+		}
+		private static bool isEmptyMemberName(Member input)
+		{
+			bool ret = String.IsNullOrEmpty(input.memberName);
+			input.callBack.setReturn(!ret, String.Empty, Constants.emptyUserName);
+			return ret;
+		}
+		private static bool isValidEmail(Member input)
+		{
+			Validation varEmail = new Validation(input.email);
+			Validate.validateEmail(varEmail);
+			bool ret = varEmail.callBack.isSuccess;
+			input.callBack.setReturn(ret, String.Empty, varEmail.callBack.message);
+			return ret;
+		}
+		private static bool isSamePassword(Member input)
+		{
+			bool ret = input.password.Equals(input.confirmPassword);
+			input.callBack.setReturn(ret, String.Empty, Constants.notSamePassword);
+			return ret;
+		}
+
 	}
 }
