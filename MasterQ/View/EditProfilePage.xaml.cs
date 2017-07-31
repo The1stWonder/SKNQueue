67c8bd84a7efd95f782af70d51d59a10a4d60885
JamesPe <koomshou@hotmail.com>
2017-07-31 17:13:38 +0700

    edit profile

--- MasterQ/View/EditProfilePage.xaml.cs
+++ MasterQ/View/EditProfilePage.xaml.cs
@@ -11,5 +11,24 @@ namespace MasterQ
 		{
 			InitializeComponent();
 		}
+
+void Submit_Clicked(object sender, System.EventArgs e)
+{
+	var name = mNameEntry.Text;
+	var lastname = mLastNameEntry.Text;
+	var Email = mEmailEntry.Text;
+	var birthdate = mBirthdateEntry.Text;
+	var Password1 = mPasswordEntry.Text;
+
+	Member member = new Member();
+	member.email = Email;
+	member.password = Password1;
+	member.firstName = name;
+	member.lastName = lastname;
+	member.birthDate = birthdate;
+
+UIReturn result = EditProfileController.getInstance().editProfile(member);
+
+		}
 	}
 }
