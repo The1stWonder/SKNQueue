32dd95cb0de6780983415b00d7b97525ed62341d
JamesPe <koomshou@hotmail.com>
2017-08-22 19:38:40 +0700

    Update History

--- MasterQ/View/HistoryPage.xaml.cs
+++ MasterQ/View/HistoryPage.xaml.cs
@@ -10,6 +10,21 @@ namespace MasterQ
 		public HistoryPage()
 		{
 			InitializeComponent();
+			getHistory();
 		}
+
+		public void getHistory()
+		{
+			Member member = new Member();
+			member.memberID = "01";
+			//UIReturn result = ShowHistoryController.getInstance().getHistory(member);
+			List<History> History = (List<History>)ShowHistoryController.getInstance().getHistory(member).returnObject;
+			foreach (History h in History)
+			{
+				mListview.ItemsSource = h.serviceID;
+				mListview.ItemsSource = h.timeAccept;
+			}
+		}
+
 	}
 }
