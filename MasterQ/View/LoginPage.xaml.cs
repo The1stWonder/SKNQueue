015d6c58c8a3e73cc009c91d48dd4d60449e4b9c
alexpook <koomshou@hotmail.com>
2017-09-04 01:40:35 +0700

    update design

--- MasterQ/View/LoginPage.xaml.cs
+++ MasterQ/View/LoginPage.xaml.cs
@@ -1,65 +1,45 @@
-﻿﻿using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace MasterQ
 {
-    public partial class LoginPage : ContentPage
-    {
-        public LoginPage()
-        {
-            InitializeComponent();
-        }
-        void Account_Clicked(object sender, System.EventArgs e)
-        {
-            Navigation.PushAsync(new RegisterPage());
-            mUsernameEntry.Text = "";
-            mPasswordEntry.Text = "";
-        }
-		//void Login_Clicked(object sender, System.EventArgs e)
-		//{
-		//    Task.Run(async () =>
-		//    {
-		//        var username = mUsernameEntry.Text;
-		//        var password = mPasswordEntry.Text;
-		//        Login MasQLogin = new Login(username, password);
-		//        UIReturn uiReturn  = await LoginController.AuthenuserAsync(MasQLogin);
-
-		//        Device.BeginInvokeOnMainThread(() =>
-		//             {
-		//            if (uiReturn.isSuccess)
-		//                 {
-		//                     Navigation.PushAsync(new MainPage());
-		//                 }
-		//                 else
-		//                 {
-		//                     DisplayAlert("Click", uiReturn.description, "Close");
-		//                 }
-		//             });
-		//    });
-		//}
-		void Login_Clicked(object sender, System.EventArgs e)
-        {
-
-            var username = mUsernameEntry.Text;
-            var password = mPasswordEntry.Text;
-            Login MasQLogin = new Login(username, password);
-            UIReturn uiReturn = LoginController.getInstance().Login(MasQLogin);
-
-            if (uiReturn.isSuccess)
-            {
-                Navigation.PushAsync(new MainPage());
-            }
-            else
-            {
-                DisplayAlert("Click", uiReturn.getDescription(), "Close");
-            }
-        }
-
-        public void OnLabelTapped(object sender, System.EventArgs args)
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage()
 		{
-			Navigation.PushAsync(new ForgetPasswordPage());
-        }
-    }
+			InitializeComponent();
+		}
+
+		public void OnLabelSignUp(object sender, System.EventArgs args)
+		{
+			Navigation.PushAsync(new RegisterPage());
+			mUsernameEntry.Text = "";
+			mPasswordEntry.Text = "";
+		}
+
+		public void OnImageSignin(object sender, System.EventArgs args)
+		{
+
+			var username = mUsernameEntry.Text;
+			var password = mPasswordEntry.Text;
+			Login MasQLogin = new Login(username, password);
+			UIReturn uiReturn = LoginController.getInstance().Login(MasQLogin);
+
+			if (uiReturn.isSuccess)
+			{
+				Navigation.PushAsync(new MainPage());
+			}
+			else
+			{
+				DisplayAlert("Click", uiReturn.getDescription(), "Close");
+			}
+		}
+
+		//public void OnLabelForgetpassword(object sender, System.EventArgs args)
+		//{
+		//	Navigation.PushAsync(new ForgetPasswordPage());
+		//}
+	}
 }
--- MasterQ/View/RegisterPage.xaml.cs
+++ MasterQ/View/RegisterPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿﻿using System;
+﻿using System;
 using System.Collections.Generic;
 
 using Xamarin.Forms;
@@ -12,24 +12,24 @@ namespace MasterQ
 			InitializeComponent();
 		}
 
-		void Submit_Clicked(object sender, System.EventArgs e)
+		public void OnImageJoin(object sender, System.EventArgs args)
 		{
 			var name = mNameEntry.Text;
-			var lastname = mLastNameEntry.Text;
+			//var lastname = mLastNameEntry.Text;
 			var Email = mEmailEntry.Text;
 			var birthdate = mBirthdateEntry.Text;
 			var Password1 = mPasswordEntry.Text;
 			var Password2 = mPassword2Entry.Text;
 
-            Member member = new Member();
-            member.email = Email;
-            member.password = Password1;
-            member.confirmPassword = Password2;
-            member.firstName = name;
-            member.lastName = lastname;
-            member.birthDate = birthdate;
+			Member member = new Member();
+			member.email = Email;
+			member.password = Password1;
+			member.confirmPassword = Password2;
+			member.firstName = name;
+			//member.lastName = lastname;
+			member.birthDate = birthdate;
 
-            UIReturn uiReturn = RegisterController.getInstance().register(member);
+			UIReturn uiReturn = RegisterController.getInstance().register(member);
 
 			if (uiReturn.isSuccess)
 			{
@@ -37,7 +37,7 @@ namespace MasterQ
 			}
 			else
 			{
-                DisplayAlert("Click", "Register Fail : "+uiReturn.getDescription(), "Close");
+				DisplayAlert("Click", "Register Fail : " + uiReturn.getDescription(), "Close");
 			}
 		}
 	}
