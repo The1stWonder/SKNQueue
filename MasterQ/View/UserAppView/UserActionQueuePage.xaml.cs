532fa4ed054aacfc7004168f8ee44842e43157a1
Sirapop Fungfuang <f.sirapop@gmail.com>
2017-09-28 21:47:00 +0700

    change ui userapp

--- MasterQ/View/UserAppView/UserActionQueuePage.xaml.cs
+++ MasterQ/View/UserAppView/UserActionQueuePage.xaml.cs
@@ -14,58 +14,76 @@ namespace MasterQ
         }
         private void InitialPage()
         {
+            CallBtn.IsVisible = true;
+            AcceptBtn.IsVisible = false;
+            SkipBtn.IsVisible = false;
+            FinishBtn.IsVisible = false;
         }
         public void OnCallTap(object sender, System.EventArgs args)
         {
-			UIReturn uiReturn = UserActionQueueController.getInstance().callQueue(UserSessionModel.choosedBranch, UserSessionModel.choosedGroup);
-			if (uiReturn.isSuccess)
-			{
-				CallQueueRs uiRes = (CallQueueRs)uiReturn.returnObject;
-				UserSessionModel.choosedQueue.tranID = uiRes.tranID;
-				qNumber.Text = uiRes.queueNumber + "";
-			}
-			else
-			{
-				DisplayAlert("Error", uiReturn.getDescription(), "cancel");
-			}
+            UIReturn uiReturn = UserActionQueueController.getInstance().callQueue(UserSessionModel.choosedBranch, UserSessionModel.choosedGroup);
+            if (uiReturn.isSuccess)
+            {
+                CallQueueRs uiRes = (CallQueueRs)uiReturn.returnObject;
+                UserSessionModel.choosedQueue.tranID = uiRes.tranID;
+                qNumber.Text = uiRes.queueNumber + "";
+                CallBtn.IsVisible = false;
+                AcceptBtn.IsVisible = true;
+                SkipBtn.IsVisible = true;
+				FinishBtn.IsVisible = false;
+            }
+            else
+            {
+                DisplayAlert("Error", uiReturn.getDescription(), "cancel");
+            }
         }
-		public void OnAcceptTap(object sender, System.EventArgs args)
-		{
-			UIReturn uiReturn = UserActionQueueController.getInstance().callQueue(UserSessionModel.choosedBranch, UserSessionModel.choosedGroup);
-			if (uiReturn.isSuccess)
-			{
-				CallQueueRs uiRes = (CallQueueRs)uiReturn.returnObject;
-				UserSessionModel.choosedQueue.tranID = uiRes.tranID;
-				qNumber.Text = uiRes.queueNumber + "";
-			}
-			else
-			{
-				DisplayAlert("Error", uiReturn.getDescription(), "cancel");
-			}
-		}
-		public void OnFinishTap(object sender, System.EventArgs args)
-		{
-			UIReturn uiReturn = UserActionQueueController.getInstance().finishQueue(UserSessionModel.choosedQueue);
-			if (uiReturn.isSuccess)
-			{
-
-			}
-			else
-			{
-				DisplayAlert("Error", uiReturn.getDescription(), "cancel");
-			}
-		}
-		public void OnSkipTap(object sender, System.EventArgs args)
-		{
-			UIReturn uiReturn = UserActionQueueController.getInstance().skipQueue(UserSessionModel.choosedQueue);
-			if (uiReturn.isSuccess)
-			{
-
-			}
-			else
-			{
-				DisplayAlert("Error", uiReturn.getDescription(), "cancel");
-			}
-		}
+        public void OnAcceptTap(object sender, System.EventArgs args)
+        {
+            UIReturn uiReturn = UserActionQueueController.getInstance().callQueue(UserSessionModel.choosedBranch, UserSessionModel.choosedGroup);
+            if (uiReturn.isSuccess)
+            {
+                CallQueueRs uiRes = (CallQueueRs)uiReturn.returnObject;
+                UserSessionModel.choosedQueue.tranID = uiRes.tranID;
+                qNumber.Text = uiRes.queueNumber + "";
+                CallBtn.IsVisible = false;
+				AcceptBtn.IsVisible = false;
+				SkipBtn.IsVisible = false;
+                FinishBtn.IsVisible = true;
+            }
+            else
+            {
+                DisplayAlert("Error", uiReturn.getDescription(), "cancel");
+            }
+        }
+        public void OnFinishTap(object sender, System.EventArgs args)
+        {
+            UIReturn uiReturn = UserActionQueueController.getInstance().finishQueue(UserSessionModel.choosedQueue);
+            if (uiReturn.isSuccess)
+            {
+				CallBtn.IsVisible = true;
+				AcceptBtn.IsVisible = false;
+				SkipBtn.IsVisible = false;
+				FinishBtn.IsVisible = false;
+            }
+            else
+            {
+                DisplayAlert("Error", uiReturn.getDescription(), "cancel");
+            }
+        }
+        public void OnSkipTap(object sender, System.EventArgs args)
+        {
+            UIReturn uiReturn = UserActionQueueController.getInstance().skipQueue(UserSessionModel.choosedQueue);
+            if (uiReturn.isSuccess)
+            {
+				CallBtn.IsVisible = true;
+				AcceptBtn.IsVisible = false;
+				SkipBtn.IsVisible = false;
+				FinishBtn.IsVisible = false;
+            }
+            else
+            {
+                DisplayAlert("Error", uiReturn.getDescription(), "cancel");
+            }
+        }
     }
 }
