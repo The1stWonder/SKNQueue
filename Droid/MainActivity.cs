2bd3bda372e867b50b380f88f68b49ce1566bfd4
JamesPe <koomshou@hotmail.com>
2017-11-20 19:18:37 +0700

    IpadDroid

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -12,6 +12,8 @@ namespace MasterQ.Droid
 {
 	[Activity(Label = "MasterQ.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, 
               ScreenOrientation = ScreenOrientation.Portrait)]
+              //ScreenOrientation = ScreenOrientation.Landscape)]
+
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
         public static Notification.Builder builder;
