7f23191eb1468c05395756fab1e010dde69dfe48
JamesPe <koomshou@hotmail.com>
2017-12-12 19:32:35 +0700

    fix bug

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -11,8 +11,8 @@ using Android.OS;
 namespace MasterQ.Droid
 {
 	[Activity(Label = "MasterQ.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, 
-              //ScreenOrientation = ScreenOrientation.Portrait)]
-              ScreenOrientation = ScreenOrientation.Landscape)]
+              ScreenOrientation = ScreenOrientation.Portrait)]
+              //ScreenOrientation = ScreenOrientation.Landscape)]
 
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
