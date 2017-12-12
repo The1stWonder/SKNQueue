9f0439a36649a6710f8b5bd7513a859033298b29
JamesPe <koomshou@hotmail.com>
2017-12-12 19:29:07 +0700

    Update All App Droid

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -3,16 +3,16 @@
 using Android.App;
 using Android.Content;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
+using Android.Support.V7.App;
+using Android.Util;
 using Android.Widget;
 using Android.OS;
 
 namespace MasterQ.Droid
 {
 	[Activity(Label = "MasterQ.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, 
-              ScreenOrientation = ScreenOrientation.Portrait)]
-              //ScreenOrientation = ScreenOrientation.Landscape)]
+              //ScreenOrientation = ScreenOrientation.Portrait)]
+              ScreenOrientation = ScreenOrientation.Landscape)]
 
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
