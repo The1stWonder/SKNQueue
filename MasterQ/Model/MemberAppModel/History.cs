8259ae5a7367249c75af5fabc6688169ca8c51ed
Sirapop Fungfuang <f.sirapop@gmail.com>
2017-12-28 19:02:59 +0700

    Show branchName & serviceName in History Page

--- MasterQ/Model/MemberAppModel/History.cs
+++ MasterQ/Model/MemberAppModel/History.cs
@@ -3,13 +3,15 @@ namespace MasterQ
 {
     public class History
     {
-		public string branchID { get; set; }
+        public string branchID { get; set; }
+        public string branchName { get; set; }
 		public string memberID { get; set; }
 		public DateTime queueDate { get; set; }
         public string queueNumber { get; set; }
 		public string queueType { get; set; }
 		public long ranking { get; set; }
 		public string serviceID { get; set; }
+        public string serviceName { get; set; }
 		public string timeAccept { get; set; }
 		public string timeCall { get; set; }
 		public string timeFinish { get; set; }
